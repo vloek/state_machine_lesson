6068a61a098b56f4ab4716f6cf08ccab6b8bd1f2
miom <lex.antonov@gmail.com>
2013-04-03 19:16:16 +0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StateMachineLesson::Application.config.secret_token = 'd747357d4e98b72503489e2e3d39c9a863499bf23df17a6a685a8f902be636ebf8ea4838ebccd58e261327f6ab34f33c1c58ff5537dcf910ca92c7ff241d0bea'
