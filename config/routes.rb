0e9d273da70ba8c04cdb2fbcbd7d23a860d4c0c2
miom <lex.antonov@gmail.com>
2013-04-04 16:02:12 +0400

    mini upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,7 @@ StateMachineLesson::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'posts#index'
+  root :to => 'posts#blog'
 
   # See how all your routes lay out with "rake routes"
 
